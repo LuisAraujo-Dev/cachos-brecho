1d42d41ef92d6b8fbfe67102f339803c59d5d529
Luis <luisaraujo.dev@gmail.com>
2025-10-07 18:05:25 -0300

    chore(deps): adicionar e configurar CORS para permitir comunicação com o frontend (Vite)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,27 @@
 import express from 'express';
+import cors from 'cors'
 import marcaRoutes from './routes/marcaRoutes'; 
 import pecaRoutes from './routes/pecaRoutes';
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+})); 
+
+app.use(express.json()); 
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'API Cachos-Brechó funcionando! Versão 1.0.' });
 });
 
 app.use('/api/marcas', marcaRoutes); 
-
 app.use('/api/pecas', pecaRoutes);
 
 app.listen(PORT, () => {
     console.log(`[Server] API rodando em http://localhost:${PORT}`);
-    console.log(`[Marcas] Teste GET: http://localhost:${PORT}/api/marcas`);
-    console.log(`[Pecas] Teste GET: http://localhost:${PORT}/api/pecas`);
+    console.log(`[CORS] Permitindo requisições de http://localhost:5173`);
 });
